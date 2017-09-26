e95a5076a9739139dd385dfdf44e714eb2dad9eb
Erik van Blokland <erik@letterror.com>
2017-09-26 11:39:34 +0200

    Update readme, screenshot.

--- GlyphBrowser.roboFontExt/lib/unicodeRanges.py
+++ GlyphBrowser.roboFontExt/lib/unicodeRanges.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 # Generated from glyphNameFormatter range names
-# Generated on 2017 09 25 13:06:29
+# Generated on 2017 09 26 11:34:51
 unicodeRangeNames ={   (0, 127): 'Basic Latin',
     (128, 255): 'Latin-1 Supplement',
     (256, 383): 'Latin Extended-A',
