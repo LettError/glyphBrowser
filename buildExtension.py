64fa4f1529511d1176e9fbbb3e31f324f3a95ac0
letterror <erik@letterror.com>
2021-04-26 17:52:04 +0200

    Version to 2.6.4

--- buildExtension.py
+++ buildExtension.py
@@ -60,7 +60,7 @@ if resourcesPath:
     B.icon = imagePath
 
 # version of the extension
-B.version = '2.6.3'
+B.version = '2.6.4'
 
 # should the extension be launched at start-up?
 B.launchAtStartUp = False
